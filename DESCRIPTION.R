f1078073a239d2895e640187848116778fc73de3
Zoe Meers <zoe.meers@gmail.com>
2018-05-02 13:04:40 +1000

    update

---
+++ DESCRIPTION.R
@@ -0,0 +1,21 @@
+Package: ussc_draft
+Type: Package
+Title: USSC ggplot2 themes -- dratfs
+Version: 0.0.1
+License: MIT License
+Encoding: UTF-8
+LazyData: true
+Maintainer: Zoe Meers <zoe.meers@sydney.edu.au>
+    Authors@R: person("Zoe", "Meers", role = c("aut", "cre"), email = "zoe.meers@sydney.edu.au")
+Imports:
+    ggplot2,
+readxl,
+dplyr,
+tidyr,
+readr,
+showtext
+Suggests: testthat,
+knitr,
+rmarkdown
+RoxygenNote: 6.0.1
+VignetteBuilder: knitr
